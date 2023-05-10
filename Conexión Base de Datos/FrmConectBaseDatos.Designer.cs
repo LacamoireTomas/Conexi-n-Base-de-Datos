c3b13dbb7e66ea2ee4b88d51cb0557a1a1ee222b
root <mgiraudo@ies21.edu.ar>
2023-05-10 13:38:19 -0300

    Agregar archivos de proyecto.

---
+++ Conexión Base de Datos/FrmConectBaseDatos.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Conexión_Base_de_Datos
+{
+    partial class FrmConectBaseDatos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CodigoDeProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IdGrupo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(44, 28);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(189, 89);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(283, 28);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(191, 89);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(41, 152);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(25, 13);
+            this.lblInfo.TabIndex = 2;
+            this.lblInfo.Text = "Info";
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodigoDeProducto,
+            this.NombreProducto,
+            this.IdGrupo,
+            this.Precio});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(454, 115);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // CodigoDeProducto
+            // 
+            this.CodigoDeProducto.HeaderText = "CodigoDeProducto";
+            this.CodigoDeProducto.Name = "CodigoDeProducto";
+            // 
+            // NombreProducto
+            // 
+            this.NombreProducto.HeaderText = "NombreProducto";
+            this.NombreProducto.Name = "NombreProducto";
+            // 
+            // IdGrupo
+            // 
+            this.IdGrupo.HeaderText = "IdGrupo";
+            this.IdGrupo.Name = "IdGrupo";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio";
+            this.Precio.Name = "Precio";
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Location = new System.Drawing.Point(44, 190);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(29, 13);
+            this.lblFiltro.TabIndex = 4;
+            this.lblFiltro.Text = "Filtro";
+            // 
+            // FrmConectBaseDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 339);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "FrmConectBaseDatos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmConectBaseDatos";
+            this.Load += new System.EventHandler(this.FrmConectBaseDatos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodigoDeProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdGrupo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.Label lblFiltro;
+    }
+}
+
---
+++ Conexión Base de Datos/FrmConectBaseDatos.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Conexión_Base_de_Datos
+{
+    public partial class FrmConectBaseDatos : Form
+    {
+        //Me deja conectarme a la base
+        OleDbConnection miConexionBD;
+        // Indica lo que quiero a la base de datos
+        OleDbCommand miComandoBD;
+        //para leer datos de una tabla o sql
+        OleDbDataReader miLectorBD;
+        public FrmConectBaseDatos()
+        {
+            InitializeComponent();
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miConexionBD = new OleDbConnection();
+                miConexionBD.ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:/VERDULEROS.mdb";
+                miConexionBD.Open();
+                lblInfo.Text = "Conexión Establecida";
+                lblInfo.BackColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                lblInfo.Text = "Hubo un Error" + ex.Message + ex.Source ;
+                lblInfo.BackColor = Color.Green;
+                //throw;
+            }
+
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miComandoBD = new OleDbCommand();
+
+                miComandoBD.Connection = miConexionBD;
+                miComandoBD.CommandType = CommandType.TableDirect;
+                miComandoBD.CommandText = "Productos";
+                lblInfo.Text = "Tabla Obtenida";
+                lblInfo.BackColor = Color.Green;
+
+
+                miLectorBD = miComandoBD.ExecuteReader();
+
+                while (miLectorBD.Read())
+                {
+                    dataGridView1.Rows.Add(miLectorBD[0], miLectorBD[1], miLectorBD[2], miLectorBD[3]);
+                }
+
+            }
+            catch (Exception error)
+            {
+                lblInfo.Text="hubo un error" + error.Message + error.Source ;
+                lblInfo.BackColor = Color.Red;
+
+               // throw;
+            }
+            
+        }
+
+        private void FrmConectBaseDatos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
